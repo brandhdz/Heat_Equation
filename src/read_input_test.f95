f2ed483181150566dee9b9de172479967fc42884
Brandon Adrián Hernández Juárez <abrandhdz@gmail.com>
2024-02-19 11:02:54 -0600

    Fixed read input file problem

--- src/read_input_test.f95
+++ src/read_input_test.f95
@@ -1,16 +1,13 @@
-subroutine read_input
+program read_input_test
 
-    !use iso_fortran_env, only : i4 => int32
     use parameters
 
     implicit none
-
-    !integer(i4) :: unit
 
     read(*,*) input_file
 
     open(unit = 3, file = input_file)
     read(3, nml = input_parameters)
-    !close(3)
-
-end subroutine read_input
+    write(*, nml = input_parameters)
+    
+end program read_input_test
