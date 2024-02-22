666f4ccb82ac8f098730240e7835698e95321b89
Brandon Adrián Hernández Juárez <abrandhdz@gmail.com>
2024-02-21 22:04:48 -0600

    Successful minor improvement

--- src/main.f95
+++ src/main.f95
@@ -9,8 +9,15 @@ program main
     call assign_values()
     call set_grid(t, t_if(1), k, x, x_if(1), h)
     call set_init_conditions(u, x, n, m, f_if(1), f_if(2))
-    call progressive_step()
-    !call regressive_step()
+
+    if ( method == 'Explicit' ) then
+        call progressive_step()
+    else if ( method == 'Matrix') then 
+        call regressive_step()
+    else
+        print*, "Método incorrecto"
+    end if
+    
     call write_data()
 
 end program main
--- src/parameters.f95
+++ src/parameters.f95
@@ -9,8 +9,8 @@ module parameters
     integer(i4) :: n, m, i, j
     real(dp) :: a, k, h, f, pi
     real(dp), dimension(2) :: t_if, x_if, f_if
-    character(100) :: input_file, file_name
+    character(100) :: method, input_file, file_name
 
-    namelist /input_parameters/ a, n, m, t_if, x_if, f_if, file_name
+    namelist /input_parameters/ method, a, n, m, t_if, x_if, f_if, file_name
 
 end module parameters
