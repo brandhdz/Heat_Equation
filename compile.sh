bbb2ecd3d1768b968bbcc37417ad71ed2ca07c58
Brandon Adrián Hernández Juárez <abrandhdz@gmail.com>
2024-02-29 19:37:11 -0600

    Change extensions to .f90

--- compile.sh
+++ compile.sh
@@ -4,14 +4,14 @@ SRC=./src
 OBJ=./obj
 BIN=./bin
 
-gfortran -J$OBJ -c $SRC/parameters.f95 -o $OBJ/parameters.o
-gfortran -J$OBJ -c $SRC/Crout_method.f95 -o $OBJ/Crout_method.o
-gfortran -J$OBJ -c $SRC/grid.f95 -o $OBJ/grid.o
-gfortran -J$OBJ -c $SRC/read_input.f95 -o $OBJ/read_input.o
-gfortran -J$OBJ -c $SRC/assign_values.f95 -o $OBJ/assign_values.o
-gfortran -J$OBJ -c $SRC/init_conditions.f95 -o $OBJ/init_conditions.o
-gfortran -J$OBJ -c $SRC/heat_eq_methods.f95 -o $OBJ/heat_eq_methods.o
-gfortran -J$OBJ -c $SRC/write_data.f95 -o $OBJ/write_data.o
-gfortran -J$OBJ -c $SRC/main.f95 -o $OBJ/main.o
+gfortran -J$OBJ -c $SRC/parameters.f90 -o $OBJ/parameters.o
+gfortran -J$OBJ -c $SRC/Crout_method.f90 -o $OBJ/Crout_method.o
+gfortran -J$OBJ -c $SRC/grid.f90 -o $OBJ/grid.o
+gfortran -J$OBJ -c $SRC/read_input.f90 -o $OBJ/read_input.o
+gfortran -J$OBJ -c $SRC/assign_values.f90 -o $OBJ/assign_values.o
+gfortran -J$OBJ -c $SRC/init_conditions.f90 -o $OBJ/init_conditions.o
+gfortran -J$OBJ -c $SRC/heat_eq_methods.f90 -o $OBJ/heat_eq_methods.o
+gfortran -J$OBJ -c $SRC/write_data.f90 -o $OBJ/write_data.o
+gfortran -J$OBJ -c $SRC/main.f90 -o $OBJ/main.o
 gfortran $OBJ/*.o -o $BIN/main.exe
 $BIN/main.exe <<< parameters.dat
